06ceed07e0de28b462ee7fb658f5ba4c5d1871a6
Waleed <walmalki@yahoo.com>
2024-05-31 22:02:59 +0300

    adjusted url

--- test/bookscraper/bookscraper/spiders/bookspider.py
+++ test/bookscraper/bookscraper/spiders/bookspider.py
@@ -3,15 +3,15 @@ import scrapy
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
-    allowed_domains = ["books.toscraper.com"]
-    start_urls = ["https://books.toscraper.com"]
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com']
 
     def parse(self, response):
-        books = response.css("article.product_pod")
+        books = response.css('article.product_pod')
 
         for book in books:
             yield{
-                'name' : book.css("h3 a::text").get(),
-                'price' : book.css(".product_price .price_color::text").get(),
-                'url' : book.css("h3 a").attrib["href"],
+                'name' : book.css('h3 a::text').get(),
+                'price' : book.css('.product_price .price_color::text').get(),
+                'url' : book.css('h3 a').attrib['href'],
             }
